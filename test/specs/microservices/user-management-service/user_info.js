759a188ca89bde387e6e8f0578c6a2af0c19c72b
gandhias6 <gandhias006@gmail.com>
2022-10-31 10:53:46 +0530

    2 basic changes

---
+++ test/specs/microservices/user-management-service/user_info.js
@@ -0,0 +1,27 @@
+require.("dotenv").config();
+const { expect } = require("chai");
+const util = require("./../../../../utils/microservice.util");
+const customerSerData = require("../../../../test-data/microservice-data/customer_service.json");
+
+describe("Get User Info", function(){
+    it("Get User Info with valid Data", async function(){
+        const response = util.getApiCall(`${process.env.USER_INFO}/3230`);
+        await util.mochaAddContext(this,response);
+        await expect(response.status).to.be.eql(200);
+    })
+    it("Get User Info without User ID", async function(){
+        const response = util.getApiCall(`${process.env.USER_INFO}/`);
+        await util.mochaAddContext(this,response);
+        await expect(response.status).to.be.eql(200);
+    })
+    it("Get User Info with String Data", async function(){
+        const response = util.getApiCall(`${process.env.USER_INFO}/Anand`);
+        await util.mochaAddContext(this,response);
+        await expect(response.status).to.be.eql(200);
+    })
+    it("Get User Info with Blank Data", async function(){
+        const response = util.getApiCall(`${process.env.USER_INFO}/`);
+        await util.mochaAddContext(this,response);
+        await expect(response.status).to.be.eql(200);
+    })
+})
