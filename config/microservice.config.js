c901ecec8a8f2b0c19a693068794cd6dd8bbbcec
gandhias6 <gandhias006@gmail.com>
2022-10-31 15:14:18 +0530

    Master Data Service Updated

--- config/microservice.config.js
+++ config/microservice.config.js
@@ -4,7 +4,7 @@ module.exports = {
   timeout: 90000,
   spec: [
     // "./test/specs/microservices/product-management-service/apply_crosssell.js",
-    "./test/specs/microservices/experience-layer-service/aadhar_authentication.js"
+    "./test/specs/microservices/master-data-service/city_master.js"
   ],
   reporter: "node_modules/mochawesome",
   "reporter-option": [
